ed3f1130f896b309bdfba3c99e6242e4b98e3842
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-03-24 18:14:51 -0300

    Car specification creation

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute(): void {
-    console.log('Todo');
+  private specificationsRepository: ISpecificationsRepository;
+
+  constructor(specificationsRepository: ISpecificationsRepository) {
+    this.specificationsRepository = specificationsRepository;
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specification = this.specificationsRepository.findByName(name);
+
+    if (specification) {
+      throw new Error('Specification already exists.');
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 

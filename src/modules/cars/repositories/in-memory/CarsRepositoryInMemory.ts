4c9abe2a9c20901cdbab64d075c68ebfb5974e37
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-06-30 18:06:58 -0300

    Add method to find available cars with filters

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -25,6 +25,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
       fine_amount,
       name,
       license_plate,
+      available: true,
     });
 
     this.cars.push(car);
@@ -34,6 +35,27 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
   async findByLicensePlate(license_plate: string): Promise<Car | undefined> {
     return this.cars.find(car => car.license_plate === license_plate);
+  }
+
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string,
+  ): Promise<Car[]> {
+    const cars = this.cars.filter(car => {
+      if (
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car;
+      }
+
+      return null;
+    });
+
+    return cars;
   }
 }
 

ce0e01e718d3f4d91e145a998164242072c08006
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-07-05 15:43:26 -0300

    In memory implementation for specifications repo

---
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -0,0 +1,42 @@
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
+
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from '../ISpecificationsRepository';
+
+class SpecificationsRepositoryInMemory implements ISpecificationsRepository {
+  private specifications: Specification[] = [];
+
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+    });
+
+    this.specifications.push(specification);
+
+    return specification;
+  }
+
+  async findByName(name: string): Promise<Specification | undefined> {
+    return this.specifications.find(
+      specification => specification.name === name,
+    );
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const filteredSpecifications = this.specifications.filter(specification =>
+      ids.includes(specification.id),
+    );
+
+    return filteredSpecifications;
+  }
+}
+
+export { SpecificationsRepositoryInMemory };

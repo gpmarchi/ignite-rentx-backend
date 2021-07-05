bb0156e7a38f110e7b3d48aa497ee05c47189a49
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-07-05 15:48:30 -0300

    Insert specifications association with car

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,16 +1,21 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { SpecificationsRepositoryInMemory } from '@modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory';
 import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarSpecificationUseCase } from './CreateCarSpecificationUseCase';
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
 let carsRepositoryInMemory: CarsRepositoryInMemory;
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
 
 describe('Create car specification', () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory();
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
+
     createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
       carsRepositoryInMemory,
+      specificationsRepositoryInMemory,
     );
   });
 
@@ -25,12 +30,20 @@ describe('Create car specification', () => {
       category_id: 'categoryId',
     });
 
-    const specifications_ids = ['54321'];
+    const specification = await specificationsRepositoryInMemory.create({
+      description: 'test',
+      name: 'test',
+    });
 
-    await createCarSpecificationUseCase.execute({
+    const specifications_ids = [specification.id];
+
+    const specificationsCars = await createCarSpecificationUseCase.execute({
       car_id: car.id,
       specifications_ids,
     });
+
+    expect(specificationsCars).toHaveProperty('specifications');
+    expect(specificationsCars.specifications.length).toBe(1);
   });
 
   it('should not be able to add specification to nonexistent car', async () => {
--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from 'tsyringe';
 
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
@@ -8,23 +10,38 @@ interface IRequest {
   specifications_ids: string[];
 }
 
-// @injectable()
+@injectable()
 class CreateCarSpecificationUseCase {
   private carsRepository: ICarsRepository;
 
+  private specificationsRepository: ISpecificationsRepository;
+
   constructor(
-    // @inject('CarsRepository')
+    @inject('CarsRepository')
     carsRepository: ICarsRepository,
+    @inject('SpecificationsRepository')
+    specificationsRepository: ISpecificationsRepository,
   ) {
     this.carsRepository = carsRepository;
+    this.specificationsRepository = specificationsRepository;
   }
 
-  async execute({ car_id, specifications_ids }: IRequest): Promise<void> {
+  async execute({ car_id, specifications_ids }: IRequest): Promise<Car> {
     const existingCar = await this.carsRepository.findById(car_id);
 
     if (!existingCar) {
       throw new AppError('Car does not exists.');
     }
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_ids,
+    );
+
+    existingCar.specifications = specifications;
+
+    await this.carsRepository.create(existingCar);
+
+    return existingCar;
   }
 }
 

81e8d39ab6f54a74d88ae4966fc227516d6c2118
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-07-05 15:47:00 -0300

    Add car id to create method to be used on update

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -14,6 +14,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
     fine_amount,
     name,
     license_plate,
+    id,
   }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
@@ -26,6 +27,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
       name,
       license_plate,
       available: true,
+      id,
     });
 
     this.cars.push(car);

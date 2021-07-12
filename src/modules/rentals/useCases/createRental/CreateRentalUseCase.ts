e813cbf1b1a0ca425c882d350271f0f4f6508e08
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-07-12 19:25:58 -0300

    Create car rental for an user

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,53 @@
+import { inject, injectable } from 'tsyringe';
+
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+// @injectable()
+class CreateRentalUseCase {
+  private rentalsRepository: IRentalsRepository;
+
+  constructor(
+    // @inject('RentalsRepository')
+    rentalsRepository: IRentalsRepository,
+  ) {
+    this.rentalsRepository = rentalsRepository;
+  }
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carRental = await this.rentalsRepository.findOpenRentalByCar(car_id);
+
+    if (carRental) {
+      throw new AppError('Car is unavailable.');
+    }
+
+    const userRental = await this.rentalsRepository.findOpenRentalByUser(
+      user_id,
+    );
+
+    if (userRental) {
+      throw new AppError('User already has an open rental.');
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };

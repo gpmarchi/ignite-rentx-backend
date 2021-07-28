e374965323236e67aece9e928ef04f591933d262
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-07-28 15:03:28 -0300

    Integration test for category listing

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,73 @@
+import { hash } from 'bcrypt';
+import request from 'supertest';
+import { Connection, getConnection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+describe('ListCategoryController', () => {
+  beforeAll(async () => {
+    connection = await createConnection('test-connection');
+
+    await connection.query('DROP TABLE IF EXISTS rentals');
+    await connection.query('DROP TABLE IF EXISTS car_images');
+    await connection.query('DROP TABLE IF EXISTS specifications_cars');
+    await connection.query('DROP TABLE IF EXISTS cars');
+    await connection.query('DROP TABLE IF EXISTS categories');
+    await connection.query('DROP TABLE IF EXISTS specifications');
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS migrations');
+
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM categories');
+    await connection.query('DELETE FROM users');
+
+    const id = uuid();
+    const password = await hash('admin', 8);
+
+    await connection.query(
+      `INSERT INTO users(id, name, email, password, is_admin, created_at, driver_license)
+				VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXX')
+			`,
+    );
+  });
+
+  afterAll(async () => {
+    const mainConnection = getConnection();
+
+    await connection.close();
+    await mainConnection.close();
+  });
+
+  it('should be able to list all categories', async () => {
+    const tokenResponse = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin',
+    });
+
+    const { token } = tokenResponse.body;
+
+    await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Category Supertest',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app).get('/categories');
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty('id');
+    expect(response.body[0].name).toEqual('Category Supertest');
+  });
+});
